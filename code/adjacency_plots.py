1cd651af96a6088f5077b05ade7e2fc074584801
salvasky <ssanchisb@uoc>
2023-10-30 16:27:09 +0100

    added adjacency plots

---
+++ code/adjacency_plots.py
@@ -0,0 +1,59 @@
+import os.path
+import glob
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+FA_matrices = [pd.read_csv(file, header=None) for file in
+               glob.glob(os.path.join('/home/vant/code/tfm1/data/FA', "*.csv"))]
+
+
+# extract labels of MS vs. HV:
+MS_labels = pd.read_csv('/home/vant/code/tfm1/data/demographics.csv')
+labels = MS_labels['mstype'].tolist()
+
+print(len(MS_labels))
+print(len(labels))
+print(labels.count(0))
+
+# use labels to separate into 2 groups:
+MS_FA = [FA_matrices[i] for i, value in enumerate(labels) if value == 0]
+HV_FA = [FA_matrices[i] for i, value in enumerate(labels) if value == -1]
+print(len(MS_FA))
+print(len(HV_FA))
+
+# Create average matrices for each of the 2 groups:
+avg_fa_ms = pd.concat(MS_FA).groupby(level=0).mean()
+avg_fa_hv = pd.concat(HV_FA).groupby(level=0).mean()
+
+# extract a matrix of a single ms patient:
+ms1 = pd.read_csv('/home/vant/code/clonemmri/data/FA/0001.csv', header=None)
+
+
+
+threshold = 0  # Adjust this value to your desired threshold
+
+avg_fa_ms_masked = avg_fa_ms.where(avg_fa_ms >= threshold, 0)
+avg_fa_hv_masked = avg_fa_hv.where(avg_fa_hv >= threshold, 0)
+ms1_masked = ms1.where(ms1 >= threshold, 0)
+
+
+fig, ax = plt.subplots(1, 2, figsize=(12, 6))  # Create two subplots side by side
+
+# Plot the first heatmap for MS
+sns.heatmap(ms1_masked, cmap='coolwarm', cbar=True, square=False, mask=None, ax=ax[0])
+ax[0].xaxis.tick_top()
+ax[0].set_title("MS FA Matrix")
+ax[0].set_xlabel("Columns")
+ax[0].set_ylabel("Rows")
+
+# Plot the second heatmap for HV
+sns.heatmap(avg_fa_hv_masked, cmap='coolwarm', cbar=True, square=False, mask=None, ax=ax[1])
+ax[1].xaxis.tick_top()
+ax[1].set_title("HV FA Matrix")
+ax[1].set_xlabel("Columns")
+ax[1].set_ylabel("")
+
+plt.show()
+
--- code/averages.py
+++ code/averages.py
@@ -1,16 +1,17 @@
 import os.path
-
 import networkx as nx
-
 import glob
 import pandas as pd
+import matplotlib.pyplot as plt
 
-FA_matrices = [pd.read_csv(file, header=None) for file in glob.glob(os.path.join('/home/vant/code/clonemmri/data/FA', "*.csv"))]
 
-#print(len(FA_matrices))
-#print(avg_all_m)
+FA_matrices = [pd.read_csv(file, header=None) for file in
+               glob.glob(os.path.join('/home/vant/code/clonemmri/data/FA', "*.csv"))]
 
-#extract labels of MS vs. HV:
+# print(len(FA_matrices))
+# print(avg_all_m)
+
+# extract labels of MS vs. HV:
 MS_labels = pd.read_csv('/home/vant/code/clonemmri/data/demographics.csv')
 labels = MS_labels['mstype'].tolist()
 
@@ -18,35 +19,34 @@ print(len(MS_labels))
 print(len(labels))
 print(labels.count(0))
 
-#use labels to separate into 2 groups:
+# use labels to separate into 2 groups:
 MS_FA = [FA_matrices[i] for i, value in enumerate(labels) if value == 0]
 HV_FA = [FA_matrices[i] for i, value in enumerate(labels) if value == -1]
 print(len(MS_FA))
 print(len(HV_FA))
 
-#Create average matrices for each of the 2 groups:
+# Create average matrices for each of the 2 groups:
 avg_fa_ms = pd.concat(MS_FA).groupby(level=0).mean()
 avg_fa_hv = pd.concat(HV_FA).groupby(level=0).mean()
 
-#extract a matrix of a single ms patient:
+# extract a matrix of a single ms patient:
 ms1 = pd.read_csv('/home/vant/code/clonemmri/data/FA/0001.csv', header=None)
 
-#print(avg_fa_ms)
+# print(avg_fa_ms)
 
-#create graphs:
+# create graphs:
 G = nx.Graph()
 H = nx.Graph()
 P1 = nx.Graph()
 
 num_nodes = avg_fa_ms.shape[0]
 
-#populate graphs with weights:
+# populate graphs with weights:
 for i in range(num_nodes):
     for j in range(i + 1, num_nodes):
         weight = avg_fa_ms.iloc[i, j]
         if weight != 0:
             G.add_edge(i, j, weight=weight)
-
 
 for i in range(num_nodes):
     for j in range(i + 1, num_nodes):
@@ -63,18 +63,17 @@ for i in range(num_nodes):
 print(H)
 print(P1)
 
-#define missing edges (difference between healthy and (avg)patient):
+# define missing edges (difference between healthy and (avg)patient):
 missing_edges = [(u, v) for u, v in H.edges() if not P1.has_edge(u, v)]
 
-#create missing edges graph:
+# create missing edges graph:
 missing_edges_graph = nx.Graph()
 missing_edges_graph.add_edges_from(missing_edges)
 
+# plotting results:
 
-#plotting results:
-import matplotlib.pyplot as plt
 
-#extract weights to create color maps:
+# extract weights to create color maps:
 ms_weights = [d["weight"] for u, v, d in P1.edges(data=True)]
 max_ms_weight = max(ms_weights)
 hv_weights = [d["weight"] for u, v, d in H.edges(data=True)]
@@ -91,20 +90,19 @@ cmap = plt.get_cmap("cividis")
 edge_colors1 = [cmap(norm(weight)) for weight in ms_weights]
 edge_colors2 = [cmap(norm(weight)) for weight in hv_weights]
 
-
-#commented lines represent weight with width instead of color (not so perceivable):
-#plot_options1 = {"node_size": 10, "with_labels": True, "width": 0.30, "edge_color":'brown'}
-#plot_options2 = {"node_size": 10, "with_labels": True, "width": 0.30, "edge_color":'green'}
+# commented lines represent weight with width instead of color (not so perceivable):
+# plot_options1 = {"node_size": 10, "with_labels": True, "width": 0.30, "edge_color":'brown'}
+# plot_options2 = {"node_size": 10, "with_labels": True, "width": 0.30, "edge_color":'green'}
 pos = nx.spring_layout(P1, iterations=15, seed=1721)
 fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(15, 9))
 ax1.axis("off")
 ax2.axis("off")
 ax3.axis("off")
-#nx.draw_networkx_edges(P1, pos, ax=ax1, width=[w / max_P1_weight * 0.3 for w in P1_weights])
+# nx.draw_networkx_edges(P1, pos, ax=ax1, width=[w / max_P1_weight * 0.3 for w in P1_weights])
 nx.draw_networkx_edges(G, pos, ax=ax1, edge_color=edge_colors1)
 nx.draw_networkx_nodes(G, pos, ax=ax1, node_size=10)
 nx.draw_networkx_labels(G, pos, ax=ax1)
-#nx.draw_networkx_edges(H, pos2, ax=ax2, width=[w / max_hv_weight * 0.3 for w in hv_weights])
+# nx.draw_networkx_edges(H, pos2, ax=ax2, width=[w / max_hv_weight * 0.3 for w in hv_weights])
 nx.draw_networkx_edges(H, pos, ax=ax2, edge_color=edge_colors2)
 nx.draw_networkx_nodes(H, pos, ax=ax2, node_size=10)
 nx.draw_networkx_labels(H, pos, ax=ax2)
@@ -113,8 +111,5 @@ nx.draw_networkx_edges(missing_edges_graph, pos, ax=ax3, width=0.3)
 nx.draw_networkx_nodes(missing_edges_graph, pos, ax=ax3, node_size=10)
 nx.draw_networkx_labels(missing_edges_graph, pos, ax=ax3)
 
-
-#nx.draw(G, with_labels=True)
+# nx.draw(G, with_labels=True)
 plt.show()
-
-
