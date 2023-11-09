8a74fac8ee2cf5e1f52ee1e18e6569bb1f059680
salvasky <ssanchisb@uoc>
2023-11-09 10:21:04 +0100

    cleaned diagonals and negative values in functional matrices

--- code/harmonize_func.py
+++ code/harmonize_func.py
@@ -12,16 +12,21 @@ csv_files_func = [file for file in sorted(os.listdir(path_func))]
 
 func_matrices = [pd.read_csv(os.path.join(path_func, file), header=None) for file in csv_files_func]
 
+# Functional matrices have weight=1 on all diagonals
+# We first convert all diagonal values to zero
 np_matrices = [m.values.reshape(76,76) for m in func_matrices]
-for m in np_matrices:
-    m[np.diag_indices_from(m)] = 0.0
 
-zero_matrices = [pd.DataFrame(m) for m in np_matrices]
+zero_matrices = [pd.DataFrame(m.copy()) for m in np_matrices]
 
-"""
-try_diag = pd.DataFrame(func_matrices[0])
-n_diag = try_diag.values.reshape((76,76))
-n_diag[np.diag_indices_from(try_diag)] = 0
+for m in zero_matrices:
+    m.values[np.diag_indices_from(m)] = 0.0
+    m.values[m.values < 0] = 0
 
-"""
+zero_matrices = [pd.DataFrame(m) for m in zero_matrices]
+
+
 print(type(zero_matrices[0]))
+print(len(zero_matrices))
+print(func_matrices[0])
+print(zero_matrices[0])
+
