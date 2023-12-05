45c043a7b46fa1be3810708858cc2c47ac1e3523
salvasky <ssanchisb@uoc>
2023-12-05 11:04:04 +0100

    correction of csv reading function affecting all code and folders

--- code/equalize_folder_lengths.py
+++ code/equalize_folder_lengths.py
@@ -11,6 +11,10 @@ destination_functional = '/home/vant/code/tfm1/data/functional'
 # Load the patient information data
 patient_info = pd.read_csv('/home/vant/code/tfm1/data/clinic.csv', usecols=['id', 'age', 'sex'])
 
+# Load data from VOLUM_NODES_CONTROLS.xls and VOLUM_NODES_PATIENTS.xls
+volum_nodes_controls = pd.read_excel('/home/vant/code/tfm1/data/VOLUM_NODES_CONTROLS.xls', header=None, skiprows=1)
+volum_nodes_patients = pd.read_excel('/home/vant/code/tfm1/data/VOLUM_NODES_PATIENTS.xls', header=None, skiprows=1)
+
 # Get the list of patient IDs
 patient_ids = patient_info['id'].astype(str)
 
@@ -21,6 +25,19 @@ csv_files_func = [file for file in os.listdir(source_functional) if file.endswit
 # Remove extra part of the file names and include only those present in clinic.csv file
 filtered_csv_files_st = [file for file in csv_files_st if file.replace('_FA_factor.csv', '') in patient_ids.values]
 filtered_csv_files_func = [file for file in csv_files_func if file.replace('_r_matrix.csv', '') in patient_ids.values]
+
+# Remove starting 'r' in all values of the first column of volum_nodes_patients and volum_nodes_controls
+volum_nodes_patients[0] = volum_nodes_patients[0].str.replace('r', '')
+volum_nodes_controls[0] = volum_nodes_controls[0].str.replace('r', '')
+
+
+# Remove rows from volum_nodes_patients where the first column value is not in patient_ids and redo the index
+volum_nodes_patients = volum_nodes_patients[volum_nodes_patients[0].isin(patient_ids.values)].reset_index(drop=True)
+
+# Export the dataframes as csv files to a folder
+volum_nodes_patients.to_csv('/home/vant/code/tfm1/data/volum_nodes_patients.csv', index=False)
+volum_nodes_controls.to_csv('/home/vant/code/tfm1/data/volum_nodes_controls.csv', index=False)
+
 
 # Create a new folder for the filtered CSV files
 os.makedirs(destination_structural, exist_ok=True)
