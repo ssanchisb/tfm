09fdffcb21f92f4cd750e1bd685be670213b01e1
salvasky <ssanchisb@uoc>
2023-12-11 16:23:19 +0100

    new comments

--- code/outliers_normalization.py
+++ code/outliers_normalization.py
@@ -1,21 +1,23 @@
 import pandas as pd
 import os.path
-from scipy.stats import iqr
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
+# In this script we will normalize the structural and functional matrices
+# and remove the outliers
+
+# Load the patient information
 patient_info = pd.read_csv('/home/vant/code/tfm1/data/clinic.csv', usecols=['id', 'age', 'sex'])
 
+# Load the structural and functional matrices
 path_st = '/home/vant/code/tfm1/data/structural_h'
 path_func = '/home/vant/code/tfm1/data/functional_h'
-
 csv_files_st = [file for file in sorted(os.listdir(path_st), key=lambda x: x.lower())]
 csv_files_func = [file for file in sorted(os.listdir(path_func), key=lambda x: x.lower())]
 
 st_matrices = [pd.read_csv(os.path.join(path_st, file), header=None) for file in csv_files_st]
 func_matrices = [pd.read_csv(os.path.join(path_func, file), header=None) for file in csv_files_func]
-
 
 fig, axes = plt.subplots(1, 2, figsize=(12, 6))
 
@@ -31,9 +33,10 @@ axes[1].set_title('Boxplot of Functional Matrices')
 plt.tight_layout()
 plt.show()
 
-
+# structural matrices normalization
 matrix_values_flattened_st = np.concatenate([df.values.flatten() for df in st_matrices])
 
+# Print the outliers and their count
 outliers_st = []
 for df in st_matrices:
     for value in df.values.flatten():
@@ -43,14 +46,16 @@ for df in st_matrices:
 print(outliers_st)
 print(len(outliers_st))
 
+# Define the upper threshold value
 percentile_to_exclude_st = 99.99
 max_value_st = np.percentile(matrix_values_flattened_st, percentile_to_exclude_st)
 print("max structural value: {}".format(max(matrix_values_flattened_st)))
 print("Upper threshold value structural: {}".format(max_value_st))
 
+# Normalize the matrices
 normalized_st_matrices = [((df - df.min().min()) / (max_value_st - df.min().min())).clip(upper=1) for df in st_matrices]
 
-# Print the outliers and their count
+# Print the resulting outliers and their count
 outliers_st = [value for value in matrix_values_flattened_st if value > max_value_st]
 
 print("Number of outliers structural:", len(outliers_st))
@@ -58,7 +63,7 @@ print("Number of outliers structural:", len(outliers_st))
 print(np.max(matrix_values_flattened_st))
 print(np.max(normalized_st_matrices))
 
-#functional matrices normalization
+# functional matrices normalization
 
 matrix_values_flattened_func = np.concatenate([df.values.flatten() for df in func_matrices])
 
@@ -69,7 +74,8 @@ percentile_to_exclude_func = 99.99
 max_value_func = np.percentile(matrix_values_flattened_func, percentile_to_exclude_func)
 print("Upper threshold value func: {}".format(max_value_func))
 
-normalized_func_matrices = [((df - df.min().min()) / (max_value_func - df.min().min())).clip(upper=1) for df in func_matrices]
+normalized_func_matrices = [((df - df.min().min()) / (max_value_func - df.min().min())).clip(upper=1) for df in
+                            func_matrices]
 normalized_func_flattened = np.concatenate([df.values.flatten() for df in normalized_func_matrices])
 
 plt.figure(figsize=(10, 6))
@@ -84,8 +90,7 @@ outliers_func = [value for value in matrix_values_flattened_func if value > max_
 print("Number of outliers func:", len(outliers_func))
 print(np.max(normalized_func_flattened))
 
-
-# Export to .csv files
+# Export normalized data to .csv files
 output_dir_st = '/home/vant/code/tfm1/data/structural_norm'
 os.makedirs(output_dir_st, exist_ok=True)
 
